5337a4037480e3125d1c10c1e61e6f3862af0335
Rohit T P <tprohit9@gmail.com>
2023-07-02 21:42:21 +0530

    no print

--- src/kernel/arch/i686/io_asm.asm
+++ src/kernel/arch/i686/io_asm.asm
@@ -29,12 +29,3 @@ global i686_DisableInterrupts
 i686_DisableInterrupts:
     cli
     ret
-
-global crash_me
-crash_me:
-    ; div by 0
-    ; mov ecx, 0x1337
-    ; mov eax, 0
-    ; div eax
-    int 0x80
-    ret
--- src/kernel/main.c
+++ src/kernel/main.c
@@ -8,15 +8,13 @@
 
 extern void _init();
 
-void crash_me();
-
 void timer(Registers* regs)
 {
     printf(".");
 }
 
 void start(BootParams* bootParams)
-{   
+{
     // call global constructors
     _init();
 
@@ -24,21 +22,14 @@ void start(BootParams* bootParams)
 
     log_debug("Main", "Boot device: %x", bootParams->BootDevice);
     log_debug("Main", "Memory region count: %d", bootParams->Memory.RegionCount);
-    for (int i = 0; i < bootParams->Memory.RegionCount; i++) 
+    for (int i = 0; i < bootParams->Memory.RegionCount; i++)
     {
-        log_debug("Main", "MEM: start=0x%llx length=0x%llx type=%x", 
+        log_debug("Main", "MEM: start=0x%llx length=0x%llx type=%x",
             bootParams->Memory.Regions[i].Begin,
             bootParams->Memory.Regions[i].Length,
             bootParams->Memory.Regions[i].Type);
     }
 
-
-    log_info("Main", "This is an info msg!");
-    log_warn("Main", "This is a warning msg!");
-    log_err("Main", "This is an error msg!");
-    log_crit("Main", "This is a critical msg!");
-    printf("Nanobyte OS v0.1\n");
-    printf("This operating system is under construction.\n");
     //i686_IRQ_RegisterHandler(0, timer);
 
     //crash_me();
